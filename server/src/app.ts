d836bf021c00d3c958241eec965da374ba336589
Arta Kjato <artakjato@gmail.com>
2026-02-24 11:10:54 +0100

    chore: MongoDb connection issues

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import { connectDB } from './config/db';
-import { env } from './config/env';  // uses our validated config!
+import { connectDB } from './config/db.js';
+import { env } from './config/env.js';  // uses our validated config!
+import dns from 'node:dns';
 
 const app = express();
 
 app.use(cors({ origin: env.CLIENT_URL }));
 app.use(express.json());
+
+
+dns.setServers(['1.1.1.1', '8.8.8.8']);
 
 // Routes will be added here in D4-T4
 
